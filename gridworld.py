70b074784dec8c0bd05754813bb53be835aa7218
jaturcotte <jaturcotte@wpi.edu>
2020-04-15 18:01:31 -0400

    Basic structure of Q Learning algorithm

--- gridworld.py
+++ gridworld.py
@@ -9,6 +9,45 @@ def runQLearning(gridworld, move_cost, move_probability):
     qTable = initializeQTable(gridworld)
     startState = (0, gridworld.shape[1] - 1)
 
+    # compute which allowed state is the most desirable to move to (highest Q value)
+    bestQ = 0
+    action = ''
+    if startState[0] - 1 >= 0 and qTable[startState]["LEFT"] > bestQ:
+        bestQ = qTable[startState]["LEFT"]
+        action = "LEFT"
+    if startState[0] + 1 < gridworld.shape[0] and qTable[startState]["RIGHT"] >= bestQ:
+        bestQ = qTable[startState]["RIGHT"]
+        action = "RIGHT"
+    if startState[1] - 1 >= 0 and qTable[startState]["UP"] > bestQ:
+        bestQ = qTable[startState]["UP"]
+        action = "UP"
+    if startState[1] + 1 < gridworld.shape[1] and qTable[startState]["DOWN"] > bestQ:
+        bestQ = qTable[startState]["DOWN"]
+        action = "DOWN"
+
+    # actually perform the move (assume no stochasticity for now)
+    if action == "LEFT":
+        newState = (startState[0] - 1, startState[1])
+    elif action == "RIGHT":
+        newState = (startState[0] + 1, startState[1])
+    elif action == "UP":
+        newState = (startState[0], startState[1] - 1)
+    else:
+        newState = (startState[0], startState[1] + 1)
+
+    # then, update Q value based on the state we ended up in
+    if action == "LEFT":
+        qTable[startState]["LEFT"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["LEFT"]))
+    elif action == "RIGHT":
+        qTable[startState]["RIGHT"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["RIGHT"]))
+    elif action == "UP":
+        qTable[startState]["UP"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["UP"]))
+    else:
+        qTable[startState]["DOWN"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["DOWN"]))
+
+    # update which state we are in
+    startState = newState
+
 
 def initializeQTable(gridworld):
     qTable = {}
