cb36d63d5d74ad82362167cd80d62e160d9ef614
Joseph Petitti <jppetitti@wpi.edu>
2020-04-16 11:49:12 -0400

    q learning mostly works

--- gridworld.py
+++ gridworld.py
@@ -1,52 +1,90 @@
 import csv
 import sys
+import random
+import time
+
 import numpy as np
+
+
+def choose_move(x, y, epsilon, qTable, gridworld):
+    # compute which allowed state is the most desirable to move to (highest Q value)
+    bestQ = float("-inf")
+    action = ''
+    if random.random() < epsilon:
+        legal_moves = []
+        if x - 1 >= 0:
+            legal_moves.append("LEFT")
+        if x + 1 < gridworld.shape[0]:
+            legal_moves.append("RIGHT")
+        if y - 1 >= 0:
+            legal_moves.append("UP")
+        if y + 1 < gridworld.shape[1]:
+            legal_moves.append("DOWN")
+        action = random.choice(legal_moves)
+        bestQ = qTable[(x, y)][action]
+    else:
+        legal_moves = []
+        if x - 1 >= 0 and qTable[(x, y)]["LEFT"] > bestQ:
+            legal_moves = ["LEFT"]
+            bestQ = qTable[(x, y)]["LEFT"]
+        elif x - 1 >= 0 and qTable[(x, y)]["LEFT"] == bestQ:
+            legal_moves.append("LEFT")
+        if x + 1 < gridworld.shape[0] and qTable[(x, y)]["RIGHT"] > bestQ:
+            legal_moves = ["RIGHT"]
+            bestQ = qTable[(x, y)]["RIGHT"]
+        elif x + 1 < gridworld.shape[0] and qTable[(x, y)]["RIGHT"] == bestQ:
+            legal_moves.append("RIGHT")
+        if y - 1 >= 0 and qTable[(x, y)]["UP"] > bestQ:
+            legal_moves = ["UP"]
+            bestQ = qTable[(x, y)]["UP"]
+        elif y - 1 >= 0 and qTable[(x, y)]["UP"] == bestQ:
+            legal_moves.append("UP")
+        if y + 1 < gridworld.shape[1] and qTable[(x, y)]["DOWN"] > bestQ:
+            legal_moves = ["DOWN"]
+            bestQ = qTable[(x, y)]["DOWN"]
+        elif y + 1 < gridworld.shape[1] and qTable[(x, y)]["DOWN"] == bestQ:
+            legal_moves.append("DOWN")
+        action = random.choice(legal_moves)
+
+    return action, bestQ
 
 
 # Given a grid world, move cost, and probability of moving correctly,
 # run the Q Learning algorithm for the given gridworld
-def runQLearning(gridworld, move_cost, move_probability):
-    qTable = initializeQTable(gridworld)
+def runQLearning(gridworld, move_cost, move_probability, qTable, epsilon):
     startState = (0, gridworld.shape[1] - 1)
+    total_move_cost = 0
+    alpha = 0.1
+    gamma = 0.9
+    policy = []
 
-    # compute which allowed state is the most desirable to move to (highest Q value)
-    bestQ = 0
-    action = ''
-    if startState[0] - 1 >= 0 and qTable[startState]["LEFT"] > bestQ:
-        bestQ = qTable[startState]["LEFT"]
-        action = "LEFT"
-    if startState[0] + 1 < gridworld.shape[0] and qTable[startState]["RIGHT"] >= bestQ:
-        bestQ = qTable[startState]["RIGHT"]
-        action = "RIGHT"
-    if startState[1] - 1 >= 0 and qTable[startState]["UP"] > bestQ:
-        bestQ = qTable[startState]["UP"]
-        action = "UP"
-    if startState[1] + 1 < gridworld.shape[1] and qTable[startState]["DOWN"] > bestQ:
-        bestQ = qTable[startState]["DOWN"]
-        action = "DOWN"
+    while True:
 
-    # actually perform the move (assume no stochasticity for now)
-    if action == "LEFT":
-        newState = (startState[0] - 1, startState[1])
-    elif action == "RIGHT":
-        newState = (startState[0] + 1, startState[1])
-    elif action == "UP":
-        newState = (startState[0], startState[1] - 1)
-    else:
-        newState = (startState[0], startState[1] + 1)
+        action, bestQ = choose_move(startState[0], startState[1], epsilon, qTable, gridworld)
 
-    # then, update Q value based on the state we ended up in
-    if action == "LEFT":
-        qTable[startState]["LEFT"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["LEFT"]))
-    elif action == "RIGHT":
-        qTable[startState]["RIGHT"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["RIGHT"]))
-    elif action == "UP":
-        qTable[startState]["UP"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["UP"]))
-    else:
-        qTable[startState]["DOWN"] += 0.1 * (move_cost + 0.9 * (bestQ - qTable[startState]["DOWN"]))
+        # actually perform the move (assume no stochasticity for now)
+        if action == "LEFT":
+            newState = (startState[0] - 1, startState[1])
+        elif action == "RIGHT":
+            newState = (startState[0] + 1, startState[1])
+        elif action == "UP":
+            newState = (startState[0], startState[1] - 1)
+        else:
+            newState = (startState[0], startState[1] + 1)
+        total_move_cost += move_cost
 
-    # update which state we are in
-    startState = newState
+        # then, update Q value based on the state we ended up in
+        future_best_q = max(qTable[newState].values())
+        qTable[startState][action] +=\
+            alpha * (move_cost + gridworld[newState[0]][newState[1]] + gamma * future_best_q - bestQ)
+
+        # update which state we are in
+        policy.append(action)
+        startState = newState
+
+        if gridworld[startState[0], startState[1]] != 0:
+            #  reached a terminal state
+            return qTable, gridworld[startState[0]][startState[1]] + total_move_cost, policy
 
 
 def initializeQTable(gridworld):
@@ -78,16 +116,45 @@ def readFromFile():
     return np.array(gridworld).T
 
 
+def printUsage():
+    print("Usage:")
+    print("\tpython gridworld FILE MOVE_COST MOVE_PROBABILITY")
+
+
 def main():
     if len(sys.argv) < 4:
-        # printUsage()
+        printUsage()
         exit()
     else:
         gridworld = readFromFile()
         move_cost = np.float_(sys.argv[2])
         move_probability = np.float_(sys.argv[3])
 
-        runQLearning(gridworld, move_cost, move_probability)
+        qTable = initializeQTable(gridworld)
+        epsilon = 0.8
+        iterations = 0
+        convergence_counter = 0
+        old_reward = float("nan")
+        start_time = time.time()
+        while True:
+            iterations += 1
+            epsilon -= 0.8 / 1000
+            qTable, reward, policy = runQLearning(gridworld, move_cost, move_probability, qTable, epsilon)
+            if reward == old_reward:
+                convergence_counter += 1
+                if convergence_counter >= 3:
+                    print("Converged after {} iterations".format(iterations))
+                    print("Expected reward: {}".format(reward))
+                    print("Policy: {}".format(policy))
+                    return
+            else:
+                old_reward = reward
+                convergence_counter = 0
+            if time.time() - start_time >= 20:
+                print("Ran out of time after {} iterations".format(iterations))
+                print("Expected reward: {}".format(reward))
+                print("Policy: {}".format(policy))
+                return
 
 
 if __name__ == '__main__':
