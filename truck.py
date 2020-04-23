3b8225d891f6f6cd2f4c35631ead3aa8534d9dab
jlfong <jlfong@wpi.edu>
2020-04-23 15:38:45 -0400

    Rewards and command lines!

--- truck.py
+++ truck.py
@@ -1,12 +1,13 @@
 import collections
 import random
+import sys
 
 
 #truck needs to know where it's at, and the packages it has
 
 
-def tick(truck, truckCapacity, probability, warehouse: collections.deque, tickcounter, numHouses):
-    print("in tick method")
+def tick(truck, truckCapacity, probability, warehouse: collections.deque, tickcounter, numHouses, startPenalty, currentScore):
+    print("tick {}".format(tickcounter))
     tickcounter += 1
     #if truck is at warehouse, packages are in warehouse and space in truck, then packages are loaded
 
@@ -43,6 +44,7 @@ def tick(truck, truckCapacity, probability, warehouse: collections.deque, tickco
             previousPackages = len(truck["packages"])
             truck["packages"] = list(filter((lambda x: x["address"] != truck["location"]), truck["packages"]))
             numDelivers = previousPackages - len(truck["packages"])
+            currentScore += numDelivers*30*numHouses
             print("delivered {} at location {}".format(numDelivers, truck["location"]))
     else:
         # TODO decide what to do next
@@ -50,20 +52,35 @@ def tick(truck, truckCapacity, probability, warehouse: collections.deque, tickco
         if roll < 0.1:
             print("decided to start deliver >:D")
             truck["delivering"] = True
-    return truck, probability, warehouse, tickcounter
+            currentScore += startPenalty
+    for package in warehouse:
+        currentScore -= tickcounter - package["timeCreated"]
+    for package in truck["packages"]:
+        currentScore -= tickcounter - package["timeCreated"]
+    print("new score is {}".format(currentScore))
+    return truck, probability, warehouse, tickcounter, currentScore
 
+def printUsage():
+    print("Usage:")
+    print("\tpython truck.py TRUCK_CAPACITY ROAD_LENGTH START_PENALTY NUM_TICKS")
 
 def main():
-    probability = 0.15
-    warehouse = collections.deque()
-    tickcounter = 0
-    numHouses = 10
-    truckCapacity = 10
-    truck = {"location": 0, "packages": [], "delivering": False}
-    timeLimit = 500
+    if len(sys.argv) < 5:
+        printUsage()
+        exit()
+    else:
+        probability = 0.15
+        warehouse = collections.deque()
+        tickcounter = 0
+        numHouses = int(sys.argv[2])
+        truckCapacity = int(sys.argv[1])
+        truck = {"location": 0, "packages": [], "delivering": False}
+        timeLimit = int(sys.argv[4])
+        startPenalty = int(sys.argv[3])
+        currentScore = 0
 
-    while tickcounter < timeLimit:
-        truck, probability, warehouse, tickcounter = tick(truck, truckCapacity, probability, warehouse, tickcounter, numHouses)
+        while tickcounter < timeLimit:
+            truck, probability, warehouse, tickcounter, currentScore = tick(truck, truckCapacity, probability, warehouse, tickcounter, numHouses, startPenalty, currentScore)
 
 
 if __name__ == '__main__':
