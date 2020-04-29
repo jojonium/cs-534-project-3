541206d35314d212bef7fe1eda86c03b75ea2dea
Joseph Petitti <jppetitti@wpi.edu>
2020-04-29 03:30:24 -0400

    random tweaks and cleanup

--- truck.py
+++ truck.py
@@ -1,6 +1,5 @@
 import collections
 import random
-from pprint import pprint
 import sys
 
 
@@ -18,83 +17,75 @@ class TruckProblem:
         self.current_score = 0
         self.total_score = 0
         self.epsilon = 0
-        self.alpha = 0.1
-        self.gamma = 0.9
+        self.alpha = 0.3
+        self.gamma = 0.6
         self.start_state = (0, 0)
         self.last_action = ""
 
     def start(self):
         while self.tick_counter < self.time_limit:
             self.tick()
-            self.epsilon -= 0.4 / (self.time_limit * 0.9)
-        pprint(self.q_table.getQTable())
+        self.total_score += self.current_score
+        self.q_table.print()
         print("Final score: {}".format(self.total_score))
 
     def choose_move(self):
+        q_deliver = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))["DELIVER"]
+        q_wait = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))["WAIT"]
         if len(self.truck["packages"]) == self.truck_capacity:
-            print("full truck")
-            print(packageDistribution(self.truck["packages"], self.num_houses))
+            # always deliver if truck is full
             action = "DELIVER"
-            best_q = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))[action]
+            best_q = q_deliver
         elif random.random() < self.epsilon:
-            print("please don't do this")
+            # chance for random explore
             if random.random() < 0.5:
                 action = "WAIT"
+                best_q = q_wait
             else:
                 action = "DELIVER"
-            #best_q = self.q_table[(len(self.truck["packages"]), self.probability)][action]
-            best_q = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))[action]
+                best_q = q_deliver
         else:
-            # best_q = self.q_table[(len(self.truck["packages"]), self.probability)]["DELIVER"]
-            best_q = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))["DELIVER"]
-            action = "DELIVER"
-            # if self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))["WAIT"] == best_q:
-            # # if self.q_table[(len(self.truck["packages"]), self.probability)]["WAIT"] == best_q:
-            #     if random.random() < 0.5:
-            #         action = "WAIT"
-            #     else:
-            #         action = "DELIVER"
-            #     # best_q = self.q_table[(len(self.truck["packages"]), self.probability)][action]
-            #     best_q = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))[action]
-            if self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))["WAIT"] >= best_q:
-            # elif self.q_table[(len(self.truck["packages"]), self.probability)]["WAIT"] >= best_q:
+            if q_deliver > q_wait:
+                action = "DELIVER"
+                best_q = q_deliver
+            elif q_deliver == q_wait:
+                # tied, choose randomly
+                if len(self.truck["packages"]) > 0 and random.random() < 0.25:
+                    action = "DELIVER"
+                    best_q = q_deliver
+                else:
+                    action = "WAIT"
+                    best_q = q_wait
+            else:
                 action = "WAIT"
-                # best_q = self.q_table[(len(self.truck["packages"]), self.probability)]["WAIT"]
-                best_q = self.q_table.getEntry(packageDistribution(self.truck["packages"], self.num_houses))["WAIT"]
+                best_q = q_wait
         return action, best_q
 
     def tick(self):
         should_update = False
-        print("tick {}".format(self.tick_counter))
         self.tick_counter += 1
         # if truck is at warehouse, packages are in warehouse and space in truck, then packages are loaded
 
         if self.truck["location"] == 0:
             while len(self.truck["packages"]) < self.truck_capacity and len(self.warehouse) > 0:
-                print("loading packages at warehouse")
                 self.truck["packages"].append(self.warehouse.pop())
 
         roll = random.random()
         if roll < self.probability / 100:
             package = {"timeCreated": self.tick_counter, "address": random.randint(1, self.num_houses)}
-            print("created package for {}".format(package["address"]))
             if self.truck["location"] == 0 and len(self.truck["packages"]) < self.truck_capacity:
                 self.truck["packages"].append(package)
             else:
                 self.warehouse.append(package)
-            if self.probability < 25:
-                self.probability += 2
+            self.probability = min(25, self.probability + 2)
         else:
-            if self.probability > 5:
-                self.probability -= 2
+            self.probability = max(5, self.probability - 2)
 
         if self.truck["delivering"]:
             if len(self.truck["packages"]) == 0:
-                # print("going back to warehouse, now at location {}".format(self.truck["location"]))
                 self.truck["location"] = max(0, self.truck["location"] -1)
                 if self.truck["location"] == 0:
                     # done delivering
-                    # print("back at warehouse and not delivering")
                     should_update = True
                     self.truck["delivering"] = False
             else:
@@ -103,26 +94,14 @@ class TruckProblem:
                 self.truck["packages"] = list(filter((lambda x: x["address"] != self.truck["location"]), self.truck["packages"]))
                 numDelivers = previousPackages - len(self.truck["packages"])
                 self.current_score += numDelivers * 30 * self.num_houses
-                # print("delivered {} at location {}".format(numDelivers, self.truck["location"]))
         else:
             action, best_q = self.choose_move()
             if action == "DELIVER":
-                print("decided to start deliver >:D")
-                print(self.truck["packages"])
-                print(packageDistribution(self.truck["packages"], self.num_houses))
                 self.truck["delivering"] = True
-                # self.start_state = (len(self.truck["packages"]), self.probability)
-                # self.start_state = packageDistribution(self.truck["packages"], self.num_houses)
                 self.last_action = "DELIVER"
                 self.current_score += self.start_penalty
             else:
-                print("decided to wait")
                 should_update = True
-                print(self.truck["packages"])
-                print(packageDistribution(self.truck["packages"], self.num_houses))
-                # decided to wait, update table
-                # self.start_state = (len(self.truck["packages"]), self.probability)
-                # self.start_state = packageDistribution(self.truck["packages"], self.num_houses)
                 self.last_action = "WAIT"
             self.start_state = packageDistribution(self.truck["packages"], self.num_houses)
         for package in self.warehouse:
@@ -132,18 +111,13 @@ class TruckProblem:
 
         if should_update:
             self.updateTable()
-        # if self.truck["location"] == 0:
-        #     self.updateTable()
-            # print("new score is {}".format(self.total_score))
         return
 
     def updateTable(self):
-        # print("UPDATING TABLE")
-        print("start state in updateTable is {}".format(self.start_state))
-        print("current score is {}".format(self.current_score))
         new_state = packageDistribution(self.truck["packages"], self.num_houses)
         future_best_q = max(self.q_table.getEntry(new_state)["WAIT"], self.q_table.getEntry(new_state)["DELIVER"])
-        new_value = self.alpha * (self.current_score + self.gamma * future_best_q - self.q_table.getEntry(self.start_state)[self.last_action])
+        old_value = self.q_table.getEntry(self.start_state)[self.last_action]
+        new_value = old_value + self.alpha * (self.current_score + self.gamma * future_best_q - old_value)
         self.q_table.updateEntry(self.start_state, self.last_action, new_value)
         self.total_score += self.current_score
         self.current_score = 0
@@ -166,10 +140,21 @@ class QTable:
     def getQTable(self):
         return self.q_table
 
+    def print(self):
+        prefer_deliver = []
+        prefer_wait = []
+        for key, value in self.q_table.items():
+            if value["DELIVER"] > value["WAIT"]:
+                prefer_deliver.append(key)
+            else:
+                prefer_wait.append(key)
+        print("States where we prefer to wait: {}".format(prefer_wait))
+        print("States where we prefer to deliver: {}".format(prefer_deliver))
+
 
 def packageDistribution(packages, road_length):
     # make the tuple
-    num_groups = 2
+    num_groups = 1
     package_dist = [0 for n in range(num_groups)]
     for p in packages:
         for n in range(num_groups):
